54d747d5b8a7eb03180009ae534eb180f385baf3
klack710 <willow710kut@gmail.com>
2018-06-28 20:26:16 +0900

    sw.js実装

---
+++ sw.js
@@ -0,0 +1,14 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+];
+self.addEventListener('fetch', function(event) {
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
